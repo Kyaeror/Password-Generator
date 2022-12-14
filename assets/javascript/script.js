48c07dc8d7cba0a83ed5b1b751bf67f14b3afa56
Brian Chiang <kyaerorgabootcamp@gmail.com>
2022-12-14 15:46:28 -0500

    added arrays

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,11 @@ generateBTN = document.getElementById(`generate`)
 confirmBTN = document.getElementById(`confirm`)
 numberLength = document.getElementById(`numberLength`)
 var x;
-
+lowerCase = [`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`]
+upperCase = [`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`]
+numeric = [0,1,2,3,4,5,6,7,8,9]
+specialCharacter = [` `,`!`,`"`,`#`,`$`,`%`,`&`,`'`,`(`,`)`,`*`,`+`,`-`,`.`,`/`,`:`,`;`,`<`,`=`,`>`,`?`,`@`,`[`,`]`,`^`,`_`,`{`,`}`,`|`,`~`]
+array = []
 //for loop to add 8 - 128 on select
 for (i=8; i <= 128; i++){
   option = document.createElement(`option`)
